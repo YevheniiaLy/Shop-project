b9c9ab51115fa77729d3cde6bee54786b216826a
Yevheniia <yevheniialyzun@gmail.com>
2023-05-18 20:24:14 +0300

    add react component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // React element
 //const title = React.createElement('h1', { id: 'title' }, 'Hello React')
 //JSX
-const app = (
+/* const app = (
     <div>
         <h1 id="test"> Hello app</h1>
         <p>
@@ -13,6 +13,23 @@ const app = (
             inventore accusamus illo impedit sint?
         </p>
     </div>
-)
+) */
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                laudantium possimus quas excepturi culpa eveniet commodi, nemo
+                praesentium eius esse maxime facilis, exercitationem dolorem
+                nisi inventore accusamus illo impedit sint?
+            </p>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
