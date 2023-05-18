16c4554a17500ff9870e08e68695dc9c11240e61
Yevheniia <yevheniialyzun@gmail.com>
2023-05-18 20:44:20 +0300

    add react.fragment in react component

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ import ReactDOM from 'react-dom/client'
         </p>
     </div>
 ) */
-const App = () => {
+// react component
+/* const App = () => {
     return (
         <div>
             <h1>Hello App component</h1>
@@ -26,7 +27,36 @@ const App = () => {
             </p>
         </div>
     )
+} */
+const Title = () => {
+    return <h1>Hello App component</h1>
 }
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                laudantium possimus quas excepturi culpa eveniet commodi, nemo
+                praesentium eius esse maxime facilis, exercitationem dolorem
+                nisi inventore accusamus illo impedit sint?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                laudantium possimus quas excepturi culpa eveniet commodi, nemo
+                praesentium eius esse maxime facilis, exercitationem dolorem
+                nisi inventore accusamus illo impedit sint?
+            </p>
+        </React.Fragment>
+    )
+}
+const App = () => {
+    return (
+        <div>
+            <Title /> <Content />
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
