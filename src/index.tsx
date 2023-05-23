d634ff1f824af650875af1ee3e8778fc7f10dff9
Yevheniia <yevheniialyzun@gmail.com>
2023-05-23 18:39:14 +0300

    add components

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
-// React element
-//const title = React.createElement('h1', { id: 'title' }, 'Hello React')
-//JSX
-/* const app = (
-    <div>
-        <h1 id="test"> Hello app</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-            laudantium possimus quas excepturi culpa eveniet commodi, nemo
-            praesentium eius esse maxime facilis, exercitationem dolorem nisi
-            inventore accusamus illo impedit sint?
-        </p>
-    </div>
-) */
-// react component
-/* const App = () => {
-    return (
-        <div>
-            <h1>Hello App component</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-                laudantium possimus quas excepturi culpa eveniet commodi, nemo
-                praesentium eius esse maxime facilis, exercitationem dolorem
-                nisi inventore accusamus illo impedit sint?
-            </p>
-        </div>
-    )
-} */
+
 const Title = () => {
     return <h1>Hello App component</h1>
 }
