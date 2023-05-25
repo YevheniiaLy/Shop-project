60d7acaff80c07be5e07965ec2aaee3295b5221f
Yevheniia <yevheniialyzun@gmail.com>
2023-05-25 20:06:41 +0300

    add components logo and menu

--- src/Conteiner/Header/Header.tsx
+++ src/Conteiner/Header/Header.tsx
@@ -1,11 +1,13 @@
 import './Header.scss'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+import Logo from 'components/Logo/Logo'
+import Menu from 'components/Menu/Menu'
+
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
+
 type Props = {}
 const Header = (props: Props) => {
     return (
@@ -21,14 +23,8 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Logo />
+                    <Menu />
                 </Toolbar>
             </Container>
         </AppBar>
