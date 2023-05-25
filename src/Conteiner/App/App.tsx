8e853608c9b18dfc1475758dba19765751e30ae0
Yevheniia <yevheniialyzun@gmail.com>
2023-05-25 20:51:28 +0300

    add Main Pages > Home

--- src/Conteiner/App/App.tsx
+++ src/Conteiner/App/App.tsx
@@ -1,12 +1,14 @@
 import Header from 'Conteiner/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import Main from 'Conteiner/Main/Main'
 const App = () => {
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header />
+                <Main />
             </StyledEngineProvider>
         </>
     )
