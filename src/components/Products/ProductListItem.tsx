6011a3011ce968c4e3dffa8d4aede9324451566a
Yevheniia <yevheniialyzun@gmail.com>
2023-06-01 20:54:06 +0300

    add state without constructor to increment and decrement button

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -19,17 +19,18 @@ type State = {
     count: number
 }
 class ProductListItem extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            count: 1,
-        }
-        this.onIncrementClick = this.onIncrementClick.bind(this)
+    state = {
+        count: 1,
     }
 
-    onIncrementClick() {
+    onIncrementClick = () => {
         this.setState((prevState) => ({
             count: prevState.count + 1,
+        }))
+    }
+    onDecrementClick = () => {
+        this.setState((prevState) => ({
+            count: prevState.count - 1,
         }))
     }
 
@@ -48,11 +49,16 @@ class ProductListItem extends React.Component<Props, State> {
                     <div className="product-price">Price: $ {price}</div>
 
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={() => this.onDecrementClick()}
+                        >
+                            -
+                        </Button>
                         <TextField value={this.state.count} size="small" />
                         <Button
                             variant="outlined"
-                            onClick={this.onIncrementClick}
+                            onClick={() => this.onIncrementClick()}
                         >
                             +
                         </Button>
