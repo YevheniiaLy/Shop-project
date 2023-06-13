5981b138cc46842d156c3f31dab0766641eb6729
Yevheniia <yevheniialyzun@gmail.com>
2023-06-13 20:06:56 +0300

    add cart page

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,8 +1,12 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
-import Main from 'Container/Main/Main'
+
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from 'pages/Home/Home'
+import CartPage from 'pages/CartPage/CartPage'
+import { Container } from '@mui/material'
 
 type ProductsInCart = {
     [id: number]: number
@@ -25,8 +29,15 @@ const App = () => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-
-            <Main addProductToCart={addProductToCart} />
+            <Container sx={{ padding: '40px 0' }}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Container>
         </StyledEngineProvider>
     )
 }
