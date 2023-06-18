9e01fb0cdd80a02f16e606b7ca380c7f6112ce44
Yevheniia <yevheniialyzun@gmail.com>
2023-06-18 15:45:31 +0300

    add product cart in cart and add funct remoteProductFromCart

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,27 @@
-type Props = {}
-const CartPage = (props: Props) => {
-    return <div>CartPage</div>
+import { Grid } from '@mui/material'
+import CartProductList from 'components/CartProductList/CartProductList'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
+import CartTotal from 'components/CartTotal/CartTotal'
+
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
 }
+
+const CartPage = ({ productsInCart }: Props) => {
+    return (
+        <>
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+            </Grid>
+
+            <CartTotal productsInCart={productsInCart} />
+        </>
+    )
+}
+
 export default CartPage
