59131e4516f2373c78a2c04e9df1227bfcc71afb
Yevheniia <yevheniialyzun@gmail.com>
2023-06-20 20:55:18 +0300

    add redux

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -2,6 +2,9 @@ import { Button, Card, CardActions, CardContent } from '@mui/material'
 import './ProductListItem.scss'
 import { useState } from 'react'
 import Quantity from 'components/Quantity/Quantity'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
 
 type Props = {
     id: number
@@ -32,10 +35,23 @@ const ProductListItem = ({
     const onDecrementClick = () => {
         setCount((prevState) => prevState - 1)
     }
+    const isLiked = useAppSelector((state) => state.productsLikeState[id])
+    const dispatch = useAppDispatch()
 
     return (
         <Card className="product" variant="outlined">
             <CardContent>
+                <Button
+                    variant="outlined"
+                    onClick={() =>
+                        dispatch({
+                            type: 'TOGGLE_LIKE',
+                            id,
+                        })
+                    }
+                >
+                    {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                </Button>
                 <div className="product-img">
                     <img src={image} alt="" />
                 </div>
@@ -43,12 +59,11 @@ const ProductListItem = ({
                 <div className="product-description">{description}</div>
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity}Gb</div>
-
                 <div className="product-price">Price: $ {price}</div>
                 <Quantity
                     count={count}
-                    onIncrementClick={onIncrementClick}
                     onDecrementClick={onDecrementClick}
+                    onIncrementClick={onIncrementClick}
                     minCount={1}
                 />
                 <CardActions className="btn-wrap">
